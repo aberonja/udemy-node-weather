782a64e2dc92da620c940ab80030c2515e0dd55a
aberonja <aberonja@execom.eu>
2020-08-04 14:51:49 +0200

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -2,18 +2,16 @@ const weatherInfo = document.querySelector(".weather-info");
 const weatherFetch = (location = "") => {
   weatherInfo.textContent = "Loading...";
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          return (weatherInfo.textContent = data.error);
-        }
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        return (weatherInfo.textContent = data.error);
+      }
 
-        weatherInfo.textContent = `${data.forecast} ${data.location}`;
-        search.value = "";
-      });
-    }
-  );
+      weatherInfo.textContent = `${data.forecast} ${data.location}`;
+      search.value = "";
+    });
+  });
 };
 
 const weatherForm = document.querySelector("form");
